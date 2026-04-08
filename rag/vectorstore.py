6c19b4a37e8b50b8bdfbba757927b74ac563da05
BinhThanh <thanh14022005@gmail.com>
2026-04-08 18:01:12 +0700

    fix rag bug, now response is faster

--- rag/vectorstore.py
+++ rag/vectorstore.py
@@ -2,12 +2,16 @@ import chromadb
 from chromadb.utils.embedding_functions import SentenceTransformerEmbeddingFunction
 from config import CHROMA_PATH, COLLECTION_NAME, EMBEDDING_MODEL
 
+_collection = None
+
 
 def get_collection():
-    client = chromadb.PersistentClient(path=CHROMA_PATH)
-    embedding_fn = SentenceTransformerEmbeddingFunction(model_name=EMBEDDING_MODEL)
-    collection = client.get_or_create_collection(
-        name=COLLECTION_NAME,
-        embedding_function=embedding_fn,
-    )
-    return collection
+    global _collection
+    if _collection is None:
+        client = chromadb.PersistentClient(path=CHROMA_PATH)
+        embedding_fn = SentenceTransformerEmbeddingFunction(model_name=EMBEDDING_MODEL)
+        _collection = client.get_or_create_collection(
+            name=COLLECTION_NAME,
+            embedding_function=embedding_fn,
+        )
+    return _collection
